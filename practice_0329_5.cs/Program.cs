01ada77037e71a327528306a181132dc8ad8258a
qmercury <qmercury@knou.ac.kr>
2022-03-31 22:41:13 +0900

    유니티스터디 0329 연습문제5

---
+++ practice_0329_5.cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace practice_0329_5.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int j = 1;
+            while(j < 5 )
+            {      for(int i = 4; i>j; i--)
+                {
+                    Console.Write("/");
+                    
+                }        
+                
+               
+                for(int i =0; i<j; i++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine("");
+                j++;
+            }
+        }
+    }
+}
+// for와 while을 혼합하여 아래 별을 출력하기
+//   *
+//  * *
+// * * *
+//* * * *
